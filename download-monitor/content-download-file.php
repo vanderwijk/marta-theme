883a7382883b57d479bd46f4774786d9eeed2136
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2025-07-24 14:08:24 +0200

    Update content-download-file.php

--- download-monitor/content-download-file.php
+++ download-monitor/content-download-file.php
@@ -5,10 +5,17 @@
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-} // Exit if accessed directly
+}
 
-/** @var DLM_Download $dlm_download */
-?>
-<a class="download-link filetype-icon <?php echo 'filetype-' .  $dlm_download->get_the_filetype(); ?>" title="<?php if ( $dlm_download->has_version_number() ) printf( __( 'Version %s', 'download_monitor' ), $dlm_download->get_the_version_number() ); ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
-	<?php $dlm_download->the_title(); ?>
-</a> - <?php $dlm_download->the_filesize(); ?> (.<?php $dlm_download->the_filetype(); ?>)
+<div class="download-item">
+	<?php if ( has_post_thumbnail( $dlm_download->get_the_id() ) ) : ?>
+		<div class="download-thumbnail">
+			<?php echo get_the_post_thumbnail( $dlm_download->get_the_id(), 'thumbnail' ); ?>
+		</div>
+	<?php endif; ?>
+	<div class="download-content">
+		<a class="download-link filetype-icon <?php echo 'filetype-' .  $dlm_download->get_the_filetype(); ?>" title="<?php if ( $dlm_download->has_version_number() ) printf( __( 'Version %s', 'download_monitor' ), $dlm_download->get_the_version_number() ); ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+			<?php $dlm_download->the_title(); ?>
+		</a> - <?php $dlm_download->the_filesize(); ?> (.<?php $dlm_download->the_filetype(); ?>)
+	</div>
+</div>
