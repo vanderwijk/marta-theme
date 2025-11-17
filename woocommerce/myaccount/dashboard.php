20e65b66598b478273e659125e6b12121607ed92
Johan van der Wijk <johan@vanderwijk.nl>
2025-11-17 17:27:37 +0100

    Update deployment workflow and redirect script in dashboard

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -1,1 +1,6 @@
-<?php echo do_shortcode( '[downloads category="professionals" template="file"]' ); ?>
+<?php //echo do_shortcode( '[downloads category="professionals" template="file"]' ); 
+
+// redirect to https://martaonline.eu/professionals/ using javascript
+echo '<script>window.location.href = "https://martaonline.eu/professionals/";</script>';
+
+?>
